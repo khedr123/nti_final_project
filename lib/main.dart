8db587eb8ed7cc267a4881d3130694da998258d3
walaamohammed2-bit <walaamohammed604@gmail.com>
2025-11-01 22:35:22 +0200

    api

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'cubits/login/login_cubit.dart';
+import 'features/auth/repository/auth_repository.dart';
+import 'features/auth/services/auth_api_service.dart';
 import 'screens/add_service.dart';
 import 'screens/login_screen.dart';
 import 'screens/splash_screen.dart';
 import 'screens/verify_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => LoginCubit(AuthRepository(AuthApiService())),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
